05530226ad591f4493cc0b0ffea8255622d1822d
andresperezfc <felipe3apc@gmail.com>
2020-06-02 00:29:16 -0500

    validaciones añadidas

--- app/src/main/java/com/example/manillas/MainActivity.java
+++ app/src/main/java/com/example/manillas/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.manillas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -19,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private String [] monedas;
     private Spinner combo_monedas;
     private ArrayAdapter<String> adapter;
+    private TextView material, dije, tipo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
         rdOro = findViewById(R.id.rdOro);
         rdNiquel = findViewById(R.id.rdNiquel);
         rdPlata = findViewById(R.id.rdPlata);
-        cantidad = findViewById(R.id.txtcantidad);
+        cantidad = findViewById(R.id.txtCantidad);
+        material = findViewById(R.id.txtMaterial);
+        dije = findViewById(R.id.txtDije);
+        tipo = findViewById(R.id.txtTipo);
+
 
         resultado = findViewById(R.id.txtResultado);
 
@@ -47,48 +51,94 @@ public class MainActivity extends AppCompatActivity {
     public void calcular(View v){
 
         int op, divisa=1, cant;
-        cant = Integer.parseInt(cantidad.getText().toString());
+
 
         float material = 0, tipo = 0, dije = 0;
         int res;
 
         op = combo_monedas.getSelectedItemPosition();
 
-        if( rdCuero.isChecked()){
-            material = (float)(100.0/3.0);
-        }else if(rdCuerda.isChecked()){
-            material = (float)(70.0/3.0);
+        if(validar()){
+            cant = Integer.parseInt(cantidad.getText().toString());
+            if( rdCuero.isChecked()){
+                material = (float)(100.0/3.0);
+            }else if(rdCuerda.isChecked()){
+                material = (float)(70.0/3.0);
+            }
+
+            if( rdMartillo.isChecked()){
+                dije = (float)(100.0/3.0);
+            }else if(rdAncla.isChecked()){
+                dije = (float)(160.0/3.0);
+            }
+
+            if( rdOro.isChecked()){
+                tipo = (float)(100.0/3.0);
+            }else if(rdNiquel.isChecked()){
+                tipo = (float)(10.0/3.0);
+            }else if(rdPlata.isChecked()){
+                tipo = (float)(40.0/3.0);
+            }
+
+            switch(op){
+                case 0:
+                    divisa = 1;
+                    break;
+                case 1:
+                    divisa = 3200;
+                    break;
+            }
+
+            if(rdCuerda.isChecked() && rdMartillo.isChecked() && rdNiquel.isChecked()){
+                resultado.setText("50");
+            }else{
+                res = (int)(material + dije + tipo);
+                resultado.setText("$" + res*divisa*cant);
+            }
         }
 
-        if( rdMartillo.isChecked()){
-            dije = (float)(100.0/3.0);
-        }else if(rdAncla.isChecked()){
-            dije = (float)(160.0/3.0);
-        }
 
-        if( rdOro.isChecked()){
-            tipo = (float)(100.0/3.0);
-        }else if(rdNiquel.isChecked()){
-            tipo = (float)(10.0/3.0);
-        }else if(rdPlata.isChecked()){
-            tipo = (float)(40.0/3.0);
-        }
+    }
 
-        switch(op){
-            case 0:
-                divisa = 1;
-                break;
-            case 1:
-                divisa = 3200;
-                break;
-        }
+    public boolean validar(){
+        String error, cant, errorCantidad;
+        error = getResources().getString(R.string.error);
+        errorCantidad = getResources().getString(R.string.errorCantidad);
 
-        if(rdCuerda.isChecked() && rdMartillo.isChecked() && rdNiquel.isChecked()){
-            resultado.setText("50");
+
+        if(!(rdCuero.isChecked()) && !(rdCuerda.isChecked())){
+            material.setError(error);
+            material.requestFocus();
+            return false;
         }else{
-            res = (int)(material + dije + tipo);
-            resultado.setText("$" + res*divisa*cant);
+            material.setError(null);
         }
+
+        if(!(rdMartillo.isChecked()) && !(rdAncla.isChecked())){
+            dije.setError(error);
+            dije.requestFocus();
+            return false;
+        }else{
+            dije.setError(null);
+        }
+
+        if(!(rdOro.isChecked()) && !(rdNiquel.isChecked()) && !(rdPlata.isChecked())){
+            tipo.setError(error);
+            tipo.requestFocus();
+            return false;
+        }else{
+            tipo.setError(null);
+        }
+
+        if(cantidad.getText().toString().isEmpty()){
+            cantidad.setError(errorCantidad);
+            cantidad.requestFocus();
+            return false;
+        }else{
+            cantidad.setError(null);
+        }
+
+        return true;
 
     }
 
